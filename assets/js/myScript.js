2ff149031fbafe4b5f74e93db45736c65f0d39c9
Prakhar <prakhar_garg@comcast.com>
2018-11-10 18:15:17 -0500

    DONE

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -3,3 +3,31 @@ $(document).ready(function(){
     $(this).addClass("down");
   });
 });
+
+$(document).ready(function(){
+  $('.Notecard2:eq(4)').delay(5000).queue(function(){
+    $(this).addClass("down");
+  });
+});
+
+$(document).ready(function(){
+  $('.Notecard:last').delay(8000).queue(function(){
+    $(this).addClass("down");
+  });
+});
+
+$(document).ready(function(){
+  $('.Notecard2:eq(3)').delay(8000).queue(function(){
+    $(this).addClass("down");
+  });
+});
+$(document).ready(function(){
+  $('.Notecard2:eq(2)').delay(11000).queue(function(){
+    $(this).addClass("down");
+  });
+});
+$(document).ready(function(){
+  $('.Notecard2:eq(1)').delay(14000).queue(function(){
+    $(this).addClass("down");
+  });
+});
