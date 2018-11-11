7801c0ac4c77e2b377d0b27bb7a6669b79475c7f
Prakhar <prakhar_garg@comcast.com>
2018-11-10 19:51:34 -0500

    adding more flair

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -4,30 +4,45 @@ $(document).ready(function(){
   });
 });
 
-$(document).ready(function(){
-  $('.Notecard2:eq(4)').delay(5000).queue(function(){
-    $(this).addClass("down");
-  });
-});
+// $(document).ready(function(){
+//   $('.Notecard2:eq(4)').delay(5000).queue(function(){
+//     $(this).addClass("down");
+//   });
+// });
 
 $(document).ready(function(){
   $('.Notecard:last').delay(8000).queue(function(){
     $(this).addClass("down");
   });
 });
+$(document).ready(function(){
+  $('.Notecard2:eq(6)').delay(5000).queue(function(){
+    $(this).addClass("down");
+  });
+});
 
 $(document).ready(function(){
-  $('.Notecard2:eq(3)').delay(8000).queue(function(){
+  $('.Notecard2:eq(5)').delay(8000).queue(function(){
     $(this).addClass("down");
   });
 });
 $(document).ready(function(){
-  $('.Notecard2:eq(2)').delay(11000).queue(function(){
+  $('.Notecard2:eq(4)').delay(12000).queue(function(){
     $(this).addClass("down");
   });
 });
 $(document).ready(function(){
-  $('.Notecard2:eq(1)').delay(14000).queue(function(){
+  $('.Notecard2:eq(3)').delay(15000).queue(function(){
+    $(this).addClass("down");
+  });
+});
+$(document).ready(function(){
+  $('.Notecard2:eq(2)').delay(18000).queue(function(){
+    $(this).addClass("down");
+  });
+});
+$(document).ready(function(){
+  $('.Notecard2:eq(1)').delay(21000).queue(function(){
     $(this).addClass("down");
   });
 });
